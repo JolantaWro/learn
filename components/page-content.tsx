aa74c4a29945eabd491fad0653a7f83fa64ae028
JolantaWro <jolanta.woronowska@gmail.com>
2023-11-10 22:51:36 +0100

    create mainContent container

---
+++ components/page-content.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Image from 'next/image';
+import Link from "next/link";
+import mainView from "../public/puzzle.jpg"
+
+
+export function MainContent() {
+    return (
+        <>
+            <div className='container mx-auto'>
+                <section className='px-4 mt-8'>
+                    <Image className="" alt="" src={mainView} />
+                    <h2 className='font-bold text-xl mt-6'>.Thing is a platform with simple games for everyone</h2>
+                    <h3 className='text-xl '>try it!</h3>
+                </section>
+            </div>
+        </>
+    )
+}
