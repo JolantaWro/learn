be60c3a49ddcdda7dbbae3a1822ec968ed590926
JolantaWro <jolanta.woronowska@gmail.com>
2023-11-13 20:12:13 +0100

    add gap in blog

--- components/landingPage/blog-content.tsx
+++ components/landingPage/blog-content.tsx
@@ -13,14 +13,14 @@ export function BlogContent() {
         <>
             <section className='px-4 mt-8'>
                 <h3 className='font-bold text-lg text-center border shadow-md mb-6'>Blog</h3>
-                <div className='grid grid-cols-2 gap 3'>
+                <div className='grid grid-cols-2 gap-3'>
                     <div className='flex flex-col'>
                         <div className='h-[132px] w-full rounded-lg flex items-center justify-center border border-zinc-100'>
                             <Image className="" alt="" src={reading} />
                         </div>
                         
-                        <h3 className='font-bold mt-2'>Learning to read by learning global reading</h3>
-                        <p className='line-clamp-4 mt-2'>Want to learn more about global reading? Learn the definition, theoretical foundations, scientific research, effectiveness and some other interesting facts.
+                        <h3 className='font-bold mt-2 text-base'>Learning to read by learning global reading</h3>
+                        <p className='line-clamp-4 mt-2 text-base'>Want to learn more about global reading? Learn the definition, theoretical foundations, scientific research, effectiveness and some other interesting facts.
                         Global reading, the Doman method and learning to read using the Miracle Child method are sometimes confused - due to the similarity of the cards for learning to read. Meanwhile,
                         we are dealing with two concepts of child development: the Miracle Child method and the Glen Doman method.
                         </p>
@@ -29,8 +29,8 @@ export function BlogContent() {
                         <div className='h-[132px] w-full rounded-lg flex items-center justify-center border border-zinc-100'>
                             <Image className="" alt="" src={english} />
                         </div>
-                        <h3 className='font-bold mt-2'>How to teach children English from an early age?</h3>
-                        <p className='line-clamp-4 mt-2'>Do you want your child to have a better start in life and catch the bug to learn English from preschool age? 
+                        <h3 className='font-bold mt-2 text-base'>How to teach children English from an early age?</h3>
+                        <p className='line-clamp-4 mt-2 text-base'>Do you want your child to have a better start in life and catch the bug to learn English from preschool age? 
                         If you are wondering how to do it so that your child does not get discouraged, but English becomes entertainment for him - you are come to the right place. We hope to dispel all your doubts.
                         </p>
                     </div>
---
+++ components/landingPage/page-about.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Image from 'next/image';
+import Link from "next/link";
+import mainView from "../../public/puzzle.jpg"
+
+export function AboutPage() {
+    return (
+        <>
+            <section className='px-4 mt-8'>
+                <Image className="" alt="" src={mainView} />
+                <h2 className='font-bold text-xl mt-6'>Are you ready to embark on a journey that combines fun, education, and mental stimulation? Look no further! 
+                Welcome to our website, a hub for all things brain-boosting and educational.</h2>
+                <p className='text-lg'>We are dedicated to providing you with a wide array of easy, brain-puzzling games and insightful blog posts on various teaching methods, 
+                catering to learners of all ages, from infants to adults.</p> 
+                <h3 className='text-lg font-bold mt-3'>Games for All Ages</h3>
+                <p className='text-lg'>If you are a fan of games that challenge your mind and provide hours of entertainment, you are in the right place. Our collection of games is designed to stimulate your brain, 
+                    enhancing cognitive abilities such as problem-solving, critical thinking, and mathematical skills. From simple addition and subtraction games that keep your math skills sharp to captivating puzzles and brain-teasers, 
+                    there is something for everyone.</p>
+                <ul className='mt-3 list-disc text-lg'><h2 className='text-lg font-bold'>Our blog posts cover a broad range of topics, including:</h2>
+                    <li className='ml-4'>Early Childhood Education: Discover the best practices for teaching infants and toddlers, fostering their intellectual and emotional growth, and preparing them for a bright future.</li>
+                    <li className='ml-4'>K-12 Education: Find innovative ways to enhance your {`child's`} school experience, from study techniques to extracurricular activities that promote a well-rounded education.</li>
+                    <li className='ml-4'>Adult Learning: {`It's`} never too late to learn! Explore various methods and resources for adult learners, whether {`you're`} pursuing a new skill or furthering your education.</li>
+                    <li className='ml-4'>Educational Tools: Stay updated on the latest educational technologies, apps, and platforms that can make your learning journey more engaging and efficient.</li>
+                </ul>
+                <p></p>
+            </section>
+        </>
+    )
+}
