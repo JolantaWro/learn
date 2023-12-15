01f2c31ad31d5677f6d6ae172b34de5f2121b241
JolantaWro <jolanta.woronowska@gmail.com>
2023-12-15 21:15:48 +0100

    refactor code

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -4,14 +4,18 @@ import Link from "next/link";
 export default function GamesHome() {
   return (
     <main className="container mx-auto">
-        <h1>Main Page of Game</h1>
-        <p>................</p>
-        <p>Something about games words with photo</p>
-        <p>................</p>
-        <p>Link to games:</p>
-        <Link href="/games/words">Words games</Link>
-        <Link href="/not-found">Counting games</Link>
-        <Link href="/not-found">Puzzles</Link>
+      <div className="px-30  py-10 flex flex-col items-center">
+        <h1 className="text-5xl">Main Page of Game</h1>
+          <p>................</p>
+          <p>Something about games words with photo</p>
+          <p>................</p>
+          <p>Link to games:</p>
+          <Link href="/games/words">Words games</Link>
+          <Link href="/not-found">Counting games</Link>
+          <Link href="/not-found">Puzzles</Link>
+
+      </div>
+        
     </main>
   )
 }
---
+++ config/games-list.ts
@@ -0,0 +1,31 @@
+import counting from "@/public/counting.jpg"
+import words from "@/public/words.jpg"
+import blocks from "@/public/blocks.jpg"
+import story from "@/public/story.jpg"
+
+export const ListGamesData = [
+    {
+        imageUrl: counting,
+        href: "/not-found",
+        title: "Counting game",
+        description: "A game with addition, subtraction, multiplication and division depending on the level selected",
+    },
+    {
+        imageUrl: words,
+        href: "/games/words",
+        title: "Words",
+        description: "Learning words divided into different areas of life",
+    },
+    {
+        imageUrl: blocks,
+        href: "/not-found",
+        title: "Puzzles",
+        description: "Simple games for arranging or guessing pictures",
+    },
+    {
+        imageUrl: story,
+        href: "/not-found",
+        title: "Story",
+        description: "Stories for everyone to read",
+    },
+]
