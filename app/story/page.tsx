09dc971817116e8479365bdf5097b7ace50ff2c2
JolantaWro <jolanta.woronowska@gmail.com>
2023-12-19 20:26:17 +0100

    add image in component

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 
+import Image from 'next/image';
+import storyMain from "@/public/storyMainPage.jpg"
+
 
 export default function StoryHome() {
   return (
     <main className="container mx-auto">
       <div className="px-30  py-10 flex flex-col items-center">
         <h1 className="text-5xl">Main Page of .........Story.........</h1>
+          <Image src={storyMain} alt=""/>
           <p>................</p>
           <p>Something about storys in portal</p>
           <p>................</p>
