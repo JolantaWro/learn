d3fb28d684002cb4bd02452557be6df4505823ab
JolantaWro <jolanta.woronowska@gmail.com>
2024-01-05 22:36:04 +0100

    add function toggleMenu

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -1,15 +1,21 @@
 'use client'
-
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from "next/link";
 import bar from "../public/bar.svg"
 
 
 export function MainHeader() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <>
-            <div className='grid grid-cols-3 bg-zinc-200 font-bold xl:hidden'>
-                <Image className="p-1 self-center" alt="" src={bar} />
+            {/* <div className='grid grid-cols-3 bg-zinc-200 font-bold xl:hidden'>
+                <Image className="p-1 self-center" alt="menu" src={bar} />
                 <h1 className="justify-self-center font-serif p-1 text-xl">.Think</h1>
             </div>
             <div className='hidden xl:flex flex-col'>
@@ -27,6 +33,37 @@ export function MainHeader() {
                         <li><Link href="/story">Story</Link></li>
                         <li><Link href="/blog">Blog</Link></li>
                         <li><Link href="/ideas">Inspiration</Link></li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div> */}
+            <div className='grid grid-cols-3 bg-zinc-200 font-bold xl:hidden'>
+                <button className='p-1 self-center' onClick={toggleMenu}>
+                    <Image alt='menu' src={bar} />
+                </button>
+                <h1 className='justify-self-center font-serif p-1 text-xl'>.Think</h1>
+            </div>
+            <div className={`xl:flex flex-col ${isMenuOpen ? 'block' : 'hidden'}`}>
+                <div className='container mx-auto'>
+                    <h1 className='justify-self-center font-serif text-5xl font-bold p-6'>.Think</h1>
+                </div>
+                <div className='container mx-auto'>
+                    <ul className='flex flex-col items-center justify-center space-y-4 font-bold text-2xl border-y-2'>
+                        <li>
+                            <Link href='/about'>About</Link>
+                        </li>
+                        <li>
+                            <Link href='/games'>Games</Link>
+                        </li>
+                        <li>
+                            <Link href='/story'>Story</Link>
+                        </li>
+                        <li>
+                            <Link href='/blog'>Blog</Link>
+                        </li>
+                        <li>
+                            <Link href='/ideas'>Inspiration</Link>
+                        </li>
                         <li>Contact</li>
                     </ul>
                 </div>
