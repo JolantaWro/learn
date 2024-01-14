ea6f9350fac6ad96424d64cc9b288f485bd72498
JolantaWro <jolanta.woronowska@gmail.com>
2024-01-14 20:47:26 +0100

    create story 4

---
+++ app/story/baby/story-4/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { StoryN4 } from "@/components/story/story-4";
+import Image from 'next/image';
+
+
+export default function Story4() {
+  return (
+    <>
+        <StoryN4 />
+    </>
+
+  )
+}
