53104d6c3212ab86c9159dea51f8a59f75d4d937
JolantaWro <jolanta.woronowska@gmail.com>
2024-01-18 12:15:22 +0100

    change view img

--- app/story/baby/page.tsx
+++ app/story/baby/page.tsx
@@ -7,24 +7,24 @@ import Link from "next/link";
 export default function BabyMain() {
     return (
         <>
-            <Container>
-                <div className="px-30 py-10 flex flex-col items-center gap-10 col-span-12 sm:col-span-8">
-                    <h1 className="text-3xl text-center font-bold">List of stories for children</h1>
+            <div className="px-30 py-10 flex flex-col items-center gap-10">
+                <h1 className="text-3xl text-center font-bold">List of stories for children</h1>
 
-                    <div className='px-4 mt-8'>
-                        {
-                            BabyStoryData.map((data, index) => (
-                                <Story1Element
-                                    imageUrl={data.imageUrl}
-                                    href={data.href}
-                                    title={data.title}
-                                    key={`story-${index}`}
-                                />
-                            ))
-                        }
-                    </div>
+                <div className='px-4 mt-8'>
+                    {
+                        BabyStoryData.map((data, index) => (
+                            <Story1Element
+                                imageUrl={data.imageUrl}
+                                href={data.href}
+                                title={data.title}
+                                key={`story-${index}`}
+                            />
+                        ))
+                    }
                 </div>
-            </Container>
+
+
+            </div>
         </>
     )
   }
@@ -39,9 +39,9 @@ type StoryProps = {
 const Story1Element = ({ imageUrl, href, title }: StoryProps) => {
     return (
         <div className='grid grid-cols-4 pt-10'>
-            <Link href={href} className="col-span-4">
+            <Link href={href} className="xs:col-span-4 p-4 col-span-8">
                 <div className='flex flex-col gap-4'>
-                    <Image className="h-52 rounded-lg object-cover" alt="abacus" src={imageUrl} />
+                    <Image className="xs:h-60 h-[300px] rounded-lg object-cover w-[500px]" alt="" src={imageUrl} />
                     <h3 className='font-bold text-lg text-center'>{title}</h3>
                 </div>
             </Link>
