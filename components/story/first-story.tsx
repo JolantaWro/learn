3a4372f2f43a5cee8492ee6e8591d038180187b4
JolantaWro <jolanta.woronowska@gmail.com>
2024-01-22 13:42:26 +0100

    change view in xl

--- components/story/first-story.tsx
+++ components/story/first-story.tsx
@@ -11,11 +11,10 @@ export function FirstStory() {
         <>
             <section className='container mx-auto'>
                 <div className="px-30 py-10 flex flex-col items-center gap-10">
-                    <h1 className="text-3xl text-center font-bold">Little bunny Tommy</h1>
-
+                    <h1 className="text-4xl text-center font-bold ">Little bunny Tommy</h1>
                     <div className='px-4 mt-4'>
-                        <Image src={story1} alt='' className='rounded-lg object-cover"'/>
-                        <p className='text-lg pt-6'>Once upon a time, in a magical forest, lived a little bunny named Tommy. Tommy was as curious as they come. His big, floppy ears were always on high alert, catching every sound that echoed through the woods.
+                        <Image src={story1} alt='' className='rounded-lg object-cover xl:float-left pr-4'/>
+                        <p className='text-lg pt-6 xl:text-2xl xl:pt-0 leading-6 text-justify'><span className='font-semibold text-5xl'>O</span>nce upon a time, in a magical forest, lived a little bunny named Tommy. Tommy was as curious as they come. His big, floppy ears were always on high alert, catching every sound that echoed through the woods.
                         Whenever Tommy went for a stroll with his parents, he discovered new wonders. He watched colorful butterflies dance in the air, listened to the sweet melodies of singing birds, and tried to catch delicate snowflakes on his soft fur when winter arrived in the magical forest.
                         Tommy loved his life and always smiled at everything around him. His smile was like a sunbeam that brightened up the entire forest. Tommys parents were the most important beings in his world. Together, they played hide-and-seek, hopped around colorful flowers, and learned from each other.
                         Even as bedtime approached, Tommy was full of joy. He nestled comfortably in his soft nest made of moss, and Mom and Dad told him bedtime stories about the adventures of other animals in the forest. Tommy closed his big, curious eyes, dreaming of the next magical discoveries he would make the next day.
--- components/story/story-2.tsx
+++ components/story/story-2.tsx
@@ -11,11 +11,11 @@ export function StoryNr2() {
         <>
             <section className='container mx-auto'>
                 <div className="px-30 py-10 flex flex-col items-center gap-10">
-                    <h1 className="text-3xl text-center font-bold">Ruby</h1>
+                    <h1 className="text-4xl text-center font-bold">Ruby</h1>
 
                     <div className='px-4 mt-4'>
-                        <Image src={story2} alt='' className='rounded-lg object-cover"'/>
-                        <p className='text-lg pt-6'>Once upon a time, there was a little girl named Ruby who had her best friend in the whole world - a stuffed teddy bear named Paws. Paws was soft, fluffy, and always ready for a hug. Ruby and Paws were inseparable; they played together, went on walks, and took naps together. 
+                        <Image src={story2} alt='' className='rounded-lg object-cover xl:float-left pr-4'/>
+                        <p className='text-lg pt-6 xl:text-2xl xl:pt-0 leading-6 text-justify'><span className='font-semibold text-5xl'>O</span>nce upon a time, there was a little girl named Ruby who had her best friend in the whole world - a stuffed teddy bear named Paws. Paws was soft, fluffy, and always ready for a hug. Ruby and Paws were inseparable; they played together, went on walks, and took naps together. 
                     However, one day, when Ruby and her mom were in the store, Paws disappeared. Ruby felt very sad and began searching for her beloved teddy bear all over the store. Mom tried to comfort her, but for Ruby, nothing was more important than finding Paws.
                     After many searches and moments of sadness, Ruby realized that she might never find her favorite stuffed animal again. However, when they returned home, Mom found Paws under the couch. Ruby was so happy that she shouted with joy and immediately hugged him. Paws was safe and sound, ready for more adventures.
                     From that day on, Ruby and Paws were even more inseparable. The little girl promised her beloved teddy bear that she would never lose him again, and Paws promised to always be by her side, no matter what. And so, Ruby, Paws, and Mom shared many joyful moments, full of love and adventures.</p>
