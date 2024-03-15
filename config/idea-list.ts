d467ed251dc630b8761d479418507fcd5b360f4f
JolantaWro <jolanta.woronowska@gmail.com>
2024-03-15 11:10:51 +0100

    change data in idea component

---
+++ config/idea-list.ts
@@ -0,0 +1,71 @@
+import counting from "@/public/counting.jpg"
+import words from "@/public/words.jpg"
+import blocks from "@/public/blocks.jpg"
+import story1 from "@/public/story2.jpg"
+import story2 from "@/public/girlAndBear.jpg"
+import story3 from "@/public/ducks.jpg"
+import story4 from "@/public/story4.jpg"
+import story from "@/public/story.jpg"
+
+export const IdeaData = [
+    {
+        imageUrl: story1,
+        href: "/story/baby/story-1",
+        title: "Little bunny Tommy",
+    },
+    {
+        imageUrl: story2,
+        href: "/story/baby/story-2",
+        title: "Ruby",
+    },
+    {
+        imageUrl: story3,
+        href: "/story/baby/story-3",
+        title: "Duckling",
+    },
+    {
+        imageUrl: story4,
+        href: "/story/baby/story-4",
+        title: "The Brave Little Steps: Philip's Potty Adventure",
+    },
+    {
+        imageUrl: story2,
+        href: "/story/baby/story-2",
+        title: "Ruby",
+    },
+    {
+        imageUrl: story2,
+        href: "/story/baby/story-2",
+        title: "Ruby",
+    },
+    {
+        imageUrl: story2,
+        href: "/story/baby/story-2",
+        title: "Ruby",
+    },
+    {
+        imageUrl: story2,
+        href: "/story/baby/story-2",
+        title: "Ruby",
+    },
+    {
+        imageUrl: story2,
+        href: "/story/baby/story-2",
+        title: "Ruby",
+    },
+    {
+        imageUrl: story2,
+        href: "/story/baby/story-2",
+        title: "Ruby",
+    },
+    {
+        imageUrl: story3,
+        href: "/story/baby/story-3",
+        title: "Duckling",
+    },
+    {
+        imageUrl: story3,
+        href: "/story/baby/story-3",
+        title: "Duckling",
+    },
+]
