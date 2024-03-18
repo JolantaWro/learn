c058687972e92c1c5ac7f614c6086de5b3d79501
JolantaWro <jolanta.woronowska@gmail.com>
2024-03-18 20:01:28 +0100

    change intence

--- app/ideas/page.tsx
+++ app/ideas/page.tsx
@@ -3,22 +3,29 @@ import Link from "next/link";
 import { IdeaData } from '@/config/idea-list';
 import Image from "next/image";
 import { CategoryList } from "@/config/category-idea-filter";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 
 export default function IdeasHome() {
   return (
     <>
       <div className="grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12">
-        <div className="pt-10 col-span-1">
-            <div className="flex flex-col ml-20 gap-8">
+        {/* <div className="pt-10 col-span-1"> */}
+          
+            {/* <div className="flex flex-col ml-20 gap-8">
               <h1 className="p-2 text-4xl sm:invisible font-semibold">Category:</h1>
               { CategoryList.map((data, index) => (
                 <p key={data.id} className="px-4 text-2xl font-semibold hover:text-[rgba(218,164,139,1)] hover:underline hover:underline-offset-8">{data.title}</p>
               ))}
              
-            </div>
+            </div> */}
+        {/* </div> */}
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input type="email" placeholder="Email" />
+          <Button type="submit">Search</Button>
         </div>
-        <div className="pt-8 col-span-2 ">
+        <div className="pt-8 col-end-5 col-span-2 ">
           {
               IdeaData.map((data, index) => (
                   <IdeaElement
