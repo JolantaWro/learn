1235d5a4cc30addd73157f05c32b76ac42dde135
JolantaWro <jolanta.woronowska@gmail.com>
2024-03-26 22:01:19 +0100

    change means in component

--- app/ideas/detail/page.tsx
+++ app/ideas/detail/page.tsx
@@ -1,25 +1,39 @@
 
-import Image from "next/image";
+// import Image from "next/image";
 
-type InspirationProps = {
-    imageUrl: any,
-    title: string,
-    category: string,
-    description: string,
-    comment: string
+// type InspirationProps = {
+//     imageUrl: any,
+//     title: string,
+//     category: string,
+//     description: string,
+//     comment: string
+//   }
+
+
+  export  default function DetailInspiration() {
+    return (
+      <>
+        <div className="pt-8 grid xl:grid-cols-3 md:grid-cols-2 px-20 gap-10">
+
+          <h1>title</h1>
+          <p>description</p>
+          <p>category</p>
+          <div>comment</div>
+        </div>
+    </>
+    )
   }
 
-
-export function DetailInspiration({ imageUrl, title, category, description, comment }: InspirationProps) {
-  return (
-    <>
-      <div className="pt-8 grid xl:grid-cols-3 md:grid-cols-2 px-20 gap-10">
-        <Image className="" alt="" src={imageUrl} />
-        <h1>{title}</h1>
-        <p>{description}</p>
-        <p>{category}</p>
-        <div>{comment}</div>
-      </div>
-  </>
-  )
-}
+// export function DetailInspiration({ imageUrl, title, category, description, comment }: InspirationProps) {
+//   return (
+//     <>
+//       <div className="pt-8 grid xl:grid-cols-3 md:grid-cols-2 px-20 gap-10">
+//         <Image className="" alt="" src={imageUrl} />
+//         <h1>{title}</h1>
+//         <p>{description}</p>
+//         <p>{category}</p>
+//         <div>{comment}</div>
+//       </div>
+//   </>
+//   )
+// }
